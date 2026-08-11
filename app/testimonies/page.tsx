6a0295c9a066080489e6f4b521b0c937f5edc0bf
TechCodinz <divyntechcodinz@gmail.com>
2026-08-11 04:23:15 -0400

    add testimony review route

---
+++ app/testimonies/page.tsx
@@ -0,0 +1,11 @@
+import { TestimonyReviewWorkflow } from '@/components/ministry/TestimonyReviewWorkflow';
+
+export default function TestimoniesPage() {
+  return (
+    <main className="min-h-screen bg-cream-50 px-4 pb-16 pt-24 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl py-8">
+        <TestimonyReviewWorkflow />
+      </div>
+    </main>
+  );
+}
