598ac4219a054c7bcd2921c08ef2cecf2f13a614
TechCodinz <divyntechcodinz@gmail.com>
2026-08-11 04:24:12 -0400

    add outreach CRM route

---
+++ app/outreach/page.tsx
@@ -0,0 +1,11 @@
+import { OutreachMissionCRM } from '@/components/ministry/OutreachMissionCRM';
+
+export default function OutreachPage() {
+  return (
+    <main className="min-h-screen bg-cream-50 px-4 pb-16 pt-24 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl py-8">
+        <OutreachMissionCRM />
+      </div>
+    </main>
+  );
+}
