b74e9076534ccc522c3228c59b72261f377f06ec
TechCodinz <divyntechcodinz@gmail.com>
2026-08-11 04:27:44 -0400

    add church requests route

---
+++ app/requests/page.tsx
@@ -0,0 +1,11 @@
+import { ChurchRequestsDesk } from '@/components/ministry/ChurchRequestsDesk';
+
+export default function RequestsPage() {
+  return (
+    <main className="min-h-screen bg-cream-50 px-4 pb-16 pt-24 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl py-8">
+        <ChurchRequestsDesk />
+      </div>
+    </main>
+  );
+}
