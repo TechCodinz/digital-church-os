f3f0a1323155f3d806d7ec3ccb8215474ca97506
TechCodinz <divyntechcodinz@gmail.com>
2026-08-11 04:22:09 -0400

    add facilities operations route

---
+++ app/facilities/page.tsx
@@ -0,0 +1,11 @@
+import { FacilitiesAssetCommandCenter } from '@/components/ministry/FacilitiesAssetCommandCenter';
+
+export default function FacilitiesPage() {
+  return (
+    <main className="min-h-screen bg-cream-50 px-4 pb-16 pt-24 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl py-8">
+        <FacilitiesAssetCommandCenter />
+      </div>
+    </main>
+  );
+}
